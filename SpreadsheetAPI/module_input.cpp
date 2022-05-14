465056756ad2d9f582b888086e9de85904171368
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2022-05-13 23:48:07 -0400

    progress2

--- SpreadsheetAPI/module_input.cpp
+++ SpreadsheetAPI/module_input.cpp
@@ -1,7 +1,5 @@
 #include"module_input.h"
 
-std::string Values;
-
  //Constructor 
 _moduleInput::_moduleInput(){
     readFile();
@@ -12,9 +10,9 @@ double _moduleInput::convert_to_double(std::string data){
 }
 
 void _moduleInput::readFile(){
-    fstream file (FILE, ios::in);
+    std::fstream file (FILE, std::ios::in);
     if(file.is_open()){
-        file.seekg(-1,ios_base::end);                // go to one spot before the EOF
+        file.seekg(-1,std::ios_base::end);                // go to one spot before the EOF
 
         bool keepLooping = true;
         while(keepLooping) {
@@ -29,21 +27,25 @@ void _moduleInput::readFile(){
                 keepLooping = false;                // Stop at the current position.
             }
             else {                                  // If the data was neither a newline nor at the 0 byte
-                file.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+                file.seekg(-2,std::ios_base::cur);        // Move to the front of that data, then to the front of the data before it
             }
         }
 
         std::string lastLine;            
-        Values = getline(file,lastLine);                      // Read the current line
+        getline(file,lastLine);                      // Read the current line
+        Values_raw = lastLine;
         file.close();
 
     }
 }
 
 double _moduleInput::getFlowData(){
+    //Read 
+    double moduel = 5;
     return 0;
 }
 
 double _moduleInput::getPressureData(){
-    
+    double moduel = 5;
+    return 0;
 }
--- SpreadsheetAPI/module_input.h
+++ SpreadsheetAPI/module_input.h
@@ -4,23 +4,30 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 #define FILE "testdata.csv"
 
 class _moduleInput {
+    public:
+        std::string Values_raw;
+        std::string pressureD;
+        std::string flowD;
+
     public:
         //Constructor 
         _moduleInput();
 
         void readFile();
 
-    public:
         double getFlowData();
 
         double getPressureData();
 
     private:
         double convert_to_double(std::string data);
+
+        std::string sortData(std::string data);
 
 };
 
