86ac4364bd85a65dad9790b88b871ea5a600cb02
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2022-05-28 16:16:12 -0400

    updates8

--- StepperDriver/stepC.py
+++ StepperDriver/stepC.py
@@ -2,6 +2,7 @@
 
 import RPi.GPIO as GPIO
 import time as time
+import piplates.DAQCplate as DAQC
 
 # setup
 GPIO.setmode(GPIO.BCM)
@@ -11,15 +12,19 @@ GPIO.setup(18,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22,GPIO.OUT)   
 
+
+
 steps = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
-pins = [17,18,27,22]
+pins = [0,1,2,3]
 
 for x in steps:
         for y in steps:
                 if steps[x][y] == 1:
-                        GPIO.output(pins[y],GPIO.HIGH)
+                        #GPIO.output(pins[y],GPIO.HIGH)
+                        DAQC.setDOUTbit(pins[y],7)
                 else:
-                        GPIO.output(pins[y],GPIO.LOW)
+                        #GPIO.output(pins[y],GPIO.LOW)
+                        DAQC.setDOUTbit(pins[y],0)
 
 GPIO.cleanup()
 
---
+++ StepperDriver/stepper.py
@@ -0,0 +1,10 @@
+import piplates.MOTORplate as MOTOR 
+import time
+
+def toggle():
+    setDOUTbit(0, 1)
+    setDOUTbit(1, 1)
+    setDOUTbit(2, 1)
+    setDOUTbit(3, 1)
+
+    
