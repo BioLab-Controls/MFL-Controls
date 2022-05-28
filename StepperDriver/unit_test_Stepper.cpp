420675527e2095bfeb383e78419bb6f03591ba5c
Jacob B <62108812+jake161@users.noreply.github.com>
2022-05-28 02:48:11 -0400

    change5

--- StepperDriver/unit_test_Stepper.cpp
+++ StepperDriver/unit_test_Stepper.cpp
@@ -1,7 +1,9 @@
 #include"stepB.h"
+#include<wiringPi.h>
 
 int main(){
     _stepper *stepperobj = new _stepper();
+    wiringPiSetupGpio();
     stepperobj -> rotateStepper(CLOCKWISE,90);
 
     delete stepperobj;
