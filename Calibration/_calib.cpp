88cd9b144fed5d370b923a2fd888316ca537170f
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2022-05-27 21:20:04 -0400

    testing

--- Calibration/_calib.cpp
+++ Calibration/_calib.cpp
@@ -7,20 +7,20 @@ _calibration::_calibration(){
 
 //- Run the valve from open state to closed state 
 uint8_t _calibration::valveCycle(uint8_t degrees){
-    //_stepper *obj = new _stepper();
+    _stepper *obj = new _stepper();
     //If flag shows a reset
     if(FLAG == 1){
-        //obj -> rotateStepper(CLOCKWISE,degrees);
+        obj -> rotateStepper(CLOCKWISE,degrees);
     }
 }
 
 //Reset valve to open
 uint8_t _calibration::resetValve(){
     //Move position to OPEN STATE () by causing a full rotation and setting flag to OPEN
-    //_stepper *obj = new _stepper();
+    _stepper *obj = new _stepper();
     uint16_t full_rotation_back = 360;
-    //obj -> rotateStepper(ANTICLOCKWISE,full_rotation_back);
-    //delete obj;
+    obj -> rotateStepper(ANTICLOCKWISE,full_rotation_back);
+    delete obj;
     //define valve state flag to be open
     #undef FLAG
     #define FLAG 1
---
+++ StepperDriver/unit_test_Stepper.cpp
@@ -0,0 +1,7 @@
+#include"stepB.h"
+
+int main(){
+    _stepper *stepperobj = new _stepper();
+    stepperobj -> rotateStepper(CLOCKWISE,90);
+    delete calBobj;
+}
